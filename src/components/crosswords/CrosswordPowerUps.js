309d00cd06c03f8bd30aafe1258e1cfa735e433d
Brent Aronsen <bkaronsen@gmail.com>
2023-01-22 21:34:28 -0600

    Add toggle assistance

--- src/components/crosswords/CrosswordPowerUps.js
+++ src/components/crosswords/CrosswordPowerUps.js
@@ -1,37 +1,56 @@
 import "./CrosswordPowerUps.css";
+import Button from "../UI/Button";
+import { Fragment, useState } from "react";
 
 const icons = [
-  {id: "verifyWord", imgId: "checkmark", text: "Verify Word"},
-  {id: "revealRandomLetter", imgId: "die", text: "Reveal Random Letter"},
-  {id: "revealLetter", imgId: "k", text: "Reveal Letter"},
+  { id: "verifyWord", imgId: "checkmark", text: "Verify Word" },
+  { id: "revealRandomLetter", imgId: "die", text: "Reveal Random Letter" },
+  { id: "revealLetter", imgId: "k", text: "Reveal Letter" },
   // {id: "revealLetterEverywhere", imgId: "buyLetter", text: "Reveal Letter Everywhere"},
-  {id: "verifyGrid", imgId: "check", text: "Verify Grid"},
-  {id: "revealWord", imgId: "word", text: "Reveal Word"}
+  { id: "verifyGrid", imgId: "check", text: "Verify Grid" },
+  { id: "revealWord", imgId: "word", text: "Reveal Word" },
 ];
 
-const crosswordPowerUps = (props) => {
+const CrosswordPowerUps = (props) => {
+  const [showAssistance, setShowAssistance] = useState(false);
+
+  const toggleAssistance = () => {
+    setShowAssistance(!showAssistance);
+  };
+
+  return (
+    <Fragment>
+      <Button className="resetBtn" onClick={toggleAssistance}>
+        Toggle Assistance
+      </Button>
+      { showAssistance ? <PowerUps onClick={props.onClick}></PowerUps> : null}
+    </Fragment>
+  );
+};
+
+const PowerUps = (props) => {
   return (
     <div className="crosswordPowerUps">
       {icons.map((icon) => (
-        <div 
+        <div
           className="crosswordPowerUp"
           key={icon.id}
           id={icon.id}
           onClick={props.onClick}
         >
           <p>{icon.text}</p>
-          <img 
+          <img
             src={require("../../assets/icon-" + icon.imgId + ".png").default}
             alt={icon.text}
             width="50"
             height="50"
           />
-          
+
           {/* <span className='tooltiptext'>{icon.text}</span> */}
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default crosswordPowerUps;
+export default CrosswordPowerUps;
